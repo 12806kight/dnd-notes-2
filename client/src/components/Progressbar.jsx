fd146de93d5f4e998bd81f5a9ed7492ca373d529
Brewer <12806kight@gmail.com>
2023-12-06 22:16:40 -0600

    Updated the stats bar with Dungeon's Masters Stats.

--- client/src/components/Progressbar.jsx
+++ client/src/components/Progressbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import level from "../level";
 
-function Progressbar({progress = 50}) {
-    const level = 1;
+function Progressbar() {
+    let currentLevel = 0;
+    const currentExp = 1500;
+    while(currentExp >= level[currentLevel].exp){
+        currentLevel++;
+    }
+    
+    let levelExp = level[currentLevel].exp;
+    let levelPercentage = (currentExp/levelExp) * 100;
+
   return (
     <div>
-        <h1>Level {level}</h1>
+        <h1>Level {currentLevel}</h1>
     <div className='progress-bar'>
-        <div className='progress-bar-fill' style={{transform: `translate(${progress - 100}%)`}}/>
+        <div className='progress-bar-fill' style={{transform: `translate(${levelPercentage - 100}%)`}}/>    
     </div>
     </div>
   )
