95fa7a2b98f5c1db05ad0ea0ed7e105580329a15
Brewer <12806kight@gmail.com>
2023-12-24 13:54:07 -0600

    The progress bar is broken. I am not sure why.

--- client/src/components/Progressbar.jsx
+++ client/src/components/Progressbar.jsx
@@ -28,15 +28,23 @@ function Progressbar() {
      }
 
   }
-  function levelBar(){
+  function levelBar(exp){
+    console.log(exp);
     let currentLevel = 0;
-    while(exp.exp >= level[currentLevel].exp){
+    while(exp >= level[currentLevel].exp){
         currentLevel++;
     }
     
     let levelExp = level[currentLevel].exp;
-    let levelPercentage = (currentExp/levelExp) * 100;
-    return levelPercentage
+    console.log(levelExp)
+    let levelPercentage = (exp/levelExp) * 100;
+    console.log(levelPercentage)
+    return (<div className='progress-bar'>
+    <div className='pro>gress-bar-fill' 
+    style={{transform: `translate(${50 - 100}%)`}}
+    />    
+</div> 
+)
   }
   return (
 
@@ -46,19 +54,20 @@ function Progressbar() {
       <div key={exp.id}>
       <p>{exp.name}</p>
       <p>{exp.exp}</p>
-
-      <div className='progress-bar'>
-        <div className='pro>gress-bar-fill' 
-        style={{transform: `translate(${levelPercentage - 100}%)`}}
-        />    
-    </div> 
-
+      {levelBar(exp.exp)}
+      
       </div>
           ))}
+          <div className='progress-bar'>
+    <div className='pro>gress-bar-fill' 
+    style={{transform: `translate(${200 - 100}%)`}}
+    />    
+    </div> 
         <form>
             <input name="title" onChange={(e) => setNumber1(+e.target.value)} value={number1}></input>
             <button onClick={calculateTotal}>Add</button>
-    </form>
+        </form>
+        
     </div>
   )
 }
